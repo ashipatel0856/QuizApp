3798a38ae3687486dfe2f2869ac3fbfa869fd043
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-04-09 17:43:01 +0530

    changes

--- src/main/java/com/quizApp/service/AdminService.java
+++ src/main/java/com/quizApp/service/AdminService.java
@@ -1,12 +1,46 @@
 package com.quizApp.service;
 
-import com.quizApp.dto.LoginRequestDto;
+
 import com.quizApp.dto.SignupRequestDto;
 import com.quizApp.entity.Admin;
-import org.apache.coyote.BadRequestException;
+import com.quizApp.repository.AdminRepository;
 
-public interface AdminService {
-    Admin signUp(SignupRequestDto signupRequestDto);
-    String Login(LoginRequestDto loginRequestDto) throws BadRequestException;
+import com.quizApp.utils.BCrypt;
+
+import org.modelmapper.ModelMapper;
+
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+
+@Service
+public class AdminService {
+
+    private static final Logger log = Logger.getLogger(AdminService.class.getName());
+    private final AdminRepository adminRepository;
+    private final ModelMapper modelMapper;
+
+
+
+
+
+    public AdminService(AdminRepository adminRepository, ModelMapper modelMapper) {
+        this.adminRepository = adminRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public Admin signUp(SignupRequestDto signupRequestDto){
+        log.info("signup a user with email: {}"+signupRequestDto.getEmail());
+
+        boolean exists = adminRepository.existsByEmail(signupRequestDto.getEmail());
+        Admin admin = modelMapper.map(signupRequestDto, Admin.class);
+        admin.setPassword(BCrypt.hash(signupRequestDto.getPassword()));
+
+        admin=adminRepository.save(admin);
+
+        return modelMapper.map(admin, Admin.class);
+    }
+
 
 }
--- src/main/java/com/quizApp/service/JwtService.java
+++ src/main/java/com/quizApp/service/JwtService.java
@@ -1,36 +1,4 @@
 package com.quizApp.service;
 
-
-import com.quizApp.dto.LoginRequestDto;
-import com.quizApp.entity.Admin;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-
-@Service
 public class JwtService {
-
-    @Value("${jwt.secretKey}")
-    private String jwtSecretKey;
-
-    private SecretKey getSecretKey() {
-        return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
-    }
-
-    public String generateAccessToken(Admin admin) {
-        return Jwts.builder()
-                .subject(admin.getId().toString())
-                .claim("email", admin.getEmail())
-                .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000*60*100))
-                .signWith(getSecretKey())
-                .compact();
-    }
-
-
 }
